4e69af8f1cdb3136877fe38757a8d73b945c30ec
Edward Hesketh <headblockhead@gmail.com>
2022-12-30 03:42:32 +0000

    updated alacritty colors

--- edwards-laptop/root/home/headb/.config/nixpkgs/home.nix
+++ edwards-laptop/root/home/headb/.config/nixpkgs/home.nix
@@ -211,22 +211,22 @@ programs.alacritty = {
             };
             normal = {
                    black =  "#000000";
-                   red=     "#cd0000";
-                   green=   "#00cd00";
-                   yellow=  "#cdcd00";
-                   blue=    "#0000ee";
-                   magenta= "#cd00cd";
-                   cyan=    "#00cdcd";
-                   white=   "#e5e5e5";
+                   red=     "#aa0000";
+                   green=   "#00aa00";
+                   yellow=  "#aa5500";
+                   blue=    "#0000aa";
+                   magenta= "#aa00aa";
+                   cyan=    "#00aaaa";
+                   white=   "#aaaaaa";
                  };
                     bright = {
-     black=   "#7f7f7f";
-     red=     "#ff0000";
-     green=   "#00ff00";
-     yellow=  "#ffff00";
-     blue=    "#5c5cff";
-     magenta= "#ff00ff";
-     cyan =    "#00ffff";
+     black=   "#555555";
+     red=     "#ff5555";
+     green=   "#55ff55";
+     yellow=  "#ffff55";
+     blue=    "#5555ff";
+     magenta= "#ff55ff";
+     cyan =    "#55ffff";
      white = "#ffffff";
    };
           };
