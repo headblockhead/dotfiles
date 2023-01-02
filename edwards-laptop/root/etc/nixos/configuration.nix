c5eeaa9a11757ebd6c286b9946f992c16cafd88b
Edward Hesketh <headblockhead@gmail.com>
2023-01-02 19:43:19 +0000

    changed pinentry from gtk to gnome

--- edwards-laptop/root/etc/nixos/configuration.nix
+++ edwards-laptop/root/etc/nixos/configuration.nix
@@ -137,7 +137,8 @@
   programs.gnupg.agent = {
     enable = true;
     #    pinentryFlavor = "curses"; # works in console interactive only, does not work with vscode
-    pinentryFlavor = "gtk2"; # creates a GUI window always, works with vscode
+    #    pinentryFlavor = "gtk2"; # creates a gtk popup
+    pinentryFlavor = "gnome3"; # creates a gnome popup.
     enableSSHSupport = true;
   };
 
