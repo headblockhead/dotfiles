edfe2ae7633500ffa34634ed47775a0606db0257
Edward Hesketh <headblockhead@gmail.com>
2023-01-21 15:15:32 +0000

    added name to configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -101,6 +101,7 @@
     users = {
       root.hashedPassword = "!"; # Disable root login
       headb = {
+        description = "Edward Hesketh";
         isNormalUser = true;
         extraGroups =
           [ "wheel" "networkmanager" "docker" "dialout" "transmission" ];
