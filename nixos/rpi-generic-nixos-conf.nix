b254891953a0e3cc2a28c5e951f30b5c4aacbaa5
Edward Hesketh <headblockhead@gmail.com>
2023-01-29 17:07:08 +0000

    rpi: test adding xmrig

--- nixos/rpi-generic-nixos-conf.nix
+++ nixos/rpi-generic-nixos-conf.nix
@@ -71,7 +71,9 @@ users.users.pi.openssh.authorizedKeys.keys = [ sshkey ];
 
     # Enable GPU acceleration
     hardware.raspberry-pi."4".fkms-3d.enable = true;
-    
+
+    nixpkgs.config.allowUnsupportedSystem = true;
+
 services.xmrig = {
   enable = true;
   settings = {
