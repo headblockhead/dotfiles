c35f63b88a36acbaf13701cadf7571549e515b8f
Edward Hesketh <headblockhead@gmail.com>
2023-07-23 18:45:49 +0100

    add wifi to rpi-headless

--- nixos/rpi-headless-image-conf.nix
+++ nixos/rpi-headless-image-conf.nix
@@ -1,13 +1,21 @@
-{ config, pkgs, lib,... }:
+{ pkgs, lib,... }:
 
 let
   sshkey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9IxocfA5legUO3o+cbbQt75vc19UG9yrrPmWVLkwbmvGxCtEayW7ubFNXo9CwlPRqcudOARZjas3XJ4+ZwrDJC8qWrSNDSw1izZizcE5oFe/eTaw+E7jT8KcUWWfdUmyx3cuJCHUAH5HtqTXc5KtZ2PiW91lgx77CYEoDKtt14cqqKvBvWCgj8xYbuoZ5lS/tuF2TkYstxI9kNI2ibk14/YyUfPs+hVTeBCz+0/l87WePzYWwA6xkkZZzGstcpXyOKSrP/fchFC+CWs7oeoJSJ5QGkNqia6HFQrdw93BtGoD2FdR3ruNJa27lOFQcXRyijx43KVr4iLuYvdGw5TEt headb@Edwards-Laptop";
 in
   {
 
-  imports = [
-    /root/rpi-wireless.nix
-  ];
+    # wifi-config.nix example:
+
+    #networking.wireless.networks = {
+    #   "networkSSID" = {
+    #     psk = "networkPassword";
+    #   };
+    # };
+
+    modules= [
+      ./wifi-config.nix
+    ];
 
   boot = {
     tmpOnTmpfs = true;
@@ -57,13 +65,7 @@ users.users.nixos.openssh.authorizedKeys.keys = [ sshkey ];
 networking.wireless = {
     enable = true;
     userControlled.enable = true;
-    # Networks are defined in the import at the top.
-    # Example:
-    #networking.wireless.networks = {
-    #   "networkSSID" = {
-    #     psk = "networkPassword";
-    #   };
-    # };
+
     };
 
     networking.interfaces.wlan0.ipv4.addresses = [ {
