0be9ebbeba810838abb709343161e2edc8d7ffae
Edward Hesketh <headblockhead@gmail.com>
2023-08-11 21:48:12 +0100

    added nvidia options

--- nixos/compute-01-hardware.nix
+++ nixos/compute-01-hardware.nix
@@ -5,6 +5,30 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+     hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+    hardware.nvidia = {
+
+    # Modesetting is needed for most Wayland compositors
+    modesetting.enable = true;
+
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = false;
+
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
