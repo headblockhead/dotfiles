6c8c4bebdcc3e727903e836862875a994dd55ffa
Edward Hesketh <headblockhead@gmail.com>
2023-10-03 23:28:19 +0100

    remove edwards-laptop from remotebuild

--- nixos/modules/remotebuild.nix
+++ nixos/modules/remotebuild.nix
@@ -12,17 +12,6 @@
     maxJobs = 8; # 8-Core CPU
     speedFactor = 10;
   }
-    {
-    hostName = "edwards-laptop";
-    system = "x86_64-linux";
-    sshKey = "/home/headb/.ssh/id_rsa";
-    sshUser = "headb";
-    protocol = "ssh-ng";
-    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-    mandatoryFeatures = [ ];
-    maxJobs = 2; # 2-Core CPU
-    speedFactor = 3; # Slower than compute-01
-  }
 ];
   nix.distributedBuilds = true;
     nix.extraOptions = ''
