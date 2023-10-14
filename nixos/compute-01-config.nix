0fc2a2761fddcca559a72f758cd2ce4c9c763daf
Edward Hesketh <headblockhead@gmail.com>
2023-10-14 14:43:22 +0100

    feat: grub to 1920x1080

--- nixos/compute-01-config.nix
+++ nixos/compute-01-config.nix
@@ -63,7 +63,7 @@
     ''f+ /run/gdm/.config/monitors.xml - gdm gdm - ${builtins.readFile ../monitors/compute-01.xml}''
   ];
 
-    boot.loader.grub.gfxmodeEfi = "3840x2160x32";
+    boot.loader.grub.gfxmodeEfi = "1920x1080x24";
 
   nix.settings.trusted-users = [ "headb" ];
 
