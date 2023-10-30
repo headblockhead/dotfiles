48bf22cf23612f2438b79827c7ebf389c4c5fd93
Edward Hesketh <headblockhead@gmail.com>
2023-10-30 23:37:55 +0000

    fix: tcp not enabled

--- nixos/modules/sound.nix
+++ nixos/modules/sound.nix
@@ -4,7 +4,8 @@
     package = pkgs.pulseaudioFull;
     zeroconf.publish.enable = true;
     zeroconf.discovery.enable = true;
-    tcp.anonymousClients.allowedIpRanges = "192.168.155.5";
+    tcp.enable = true;
+    tcp.anonymousClients.allowAll = true;
   };
   sound.enable = true;
   security.rtkit.enable = true;
