b34c5858214b2e7194d1e586b7d643dc40e57917
Edward Hesketh <headblockhead@gmail.com>
2024-02-13 16:16:00 +0000

    fix: more router issues

--- modules/nixos/router.nix
+++ modules/nixos/router.nix
@@ -1,142 +1,21 @@
-{ pkgs, ... }:
+{pkgs,...}:
 let
-  wan_port = "enp1s0";
-  lan_port = "enp2s0";
-  iot_port = "enp3s0";
-in
+  lan_port = "enp5s0";
+  wan_port = "enp4s0";
+  iot_port = "enp8s0";
 {
-  boot.kernel = {
-    sysctl = {
-      # Linux discards network packets that are not destined for the interface they are recieved on by default. This must be changed to allow routing.
-      "net.ipv4.conf.all.forwarding" = true;
-      "net.ipv6.conf.all.forwarding" = true;
-
-      # source: https://github.com/mdlayher/homelab/blob/master/nixos/routnerr-2/configuration.nix#L52
-      # By default, not automatically configure any IPv6 addresses.
-      "net.ipv6.conf.all.accept_ra" = 0;
-      "net.ipv6.conf.all.autoconf" = 0;
-      "net.ipv6.conf.all.use_tempaddr" = 0;
-
-      # On WAN, allow IPv6 autoconfiguration and tempory address use.
-      "net.ipv6.conf.${wan_port}.accept_ra" = 2;
-      "net.ipv6.conf.${wan_port}.autoconf" = 1;
-    };
+networking.interfaces = {
+  "${lan_port}" = {
+    ipv4.address = "192.168.1.1";
   };
-
-
-  networking = {
-    useDHCP = false;
-    nat.enable = false;
-    firewall.enable = false;
-    nftables = {
-      enable = true;
-      ruleset = ''
-        table inet filter {
-          # enable flow offloading for better throughput
-          flowtable f {
-            hook ingress priority 0;
-            devices = { wan, lan };
-          }
-
-          chain output {
-            type filter hook output priority 100; policy accept;
-          }
-
-          chain input {
-            type filter hook input priority filter; policy drop;
-
-            # Allow trusted networks to access the router
-            iifname {
-              "lan",
-            } counter accept
-
-            # Allow returning traffic from wan and drop everthing else
-            iifname "wan" ct state { established, related } counter accept
-            iifname "wan drop
-          }
-
-          chain forward {
-            type filter hook forward priority filter; policy drop;
-
-            # enable flow offloading for better throughput
-            ip protocol { tcp, udp } flow offload @f
-
-            # Allow trusted network WAN access
-            iifname {
-                    "lan",
-            } oifname {
-                    "wan",
-            } counter accept comment "Allow trusted LAN to WAN"
-
-            # Allow established WAN to return
-            iifname {
-                    "wan",
-            } oifname {
-                    "lan",
-            } ct state established,related counter accept comment "Allow established back to LANs"
-          }
-        }
-
-        table ip nat {
-          chain prerouting {
-            type nat hook prerouting priority filter; policy accept;
-          }
-
-          # Setup NAT masquerading on the wan interface
-          chain postrouting {
-            type nat hook postrouting priority filter; policy accept;
-            oifname "wan" masquerade
-          }
-        }
-      '';
-    };
-    hostName = "router";
-    nameservers = [ "1.1.1.1" ];
-    # Define VLANS
-    vlans = {
-      wan = {
-        id = 10;
-        interface = "${wan_port}";
-      };
-      lan = {
-        id = 20;
-        interface = "${lan_port}";
-      };
-      iot = {
-        id = 90;
-        interface = "${iot_port}";
-      };
-    };
-    interfaces = {
-      # DHCP client on the WAN interface.
-      "${wan_port}".useDHCP = true;
-      # Don't request DHCP on the LAN and IOT interfaces.
-      "${lan_port}".useDHCP = false;
-      "${iot_port}".useDHCP = false;
-
-      # Handle the VLANs
-      wan.useDHCP = true;
-      lan = {
-        ipv4.addresses = [{
-          address = "192.168.20.1";
-          prefixLength = 24;
-        }];
-      };
-      iot = {
-        ipv4.addresses = [{
-          address = "192.168.90.1";
-          prefixLength = 24;
-        }];
-      };
-    };
+};
+ services.dnsmasq = lib.optionalAttrs config.services.hostapd.enable {
+    enable = true;
+    extraConfig = ''
+      interface=${lan_port}
+      bind-interfaces
+      dhcp-range=192.168.1.10,192.168.0.254,24h
+    '';
   };
-  environment.systemPackages = with pkgs; [
-    vim # my preferred editor
-    htop # to see the system load
-    ppp # for some manual debugging of pppd
-    ethtool # manage NIC settings (offload, NIC feeatures, ...)
-    tcpdump # view network traffic
-    conntrack-tools # view network connection states
-  ];
+  networking.bridges.br0.interfaces = [ lan_port wan_port iot_port ];
 }
-
