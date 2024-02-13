ee4981f4cc15968498069ef9cb5dcf6660062df0
Edward Hesketh <headblockhead@gmail.com>
2024-02-13 17:18:32 +0000

    fix: more router issues

--- modules/nixos/router.nix
+++ modules/nixos/router.nix
@@ -1,15 +1,26 @@
-{pkgs,...}:
+{ pkgs, config, lib, ... }:
 let
   lan_port = "enp5s0";
   wan_port = "enp4s0";
   iot_port = "enp8s0";
+in
 {
-networking.interfaces = {
-  "${lan_port}" = {
-    ipv4.address = "192.168.1.1";
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.forwarding" = true;
+    "net.ipv6.conf.all.forwarding" = false;
   };
-};
- services.dnsmasq = lib.optionalAttrs config.services.hostapd.enable {
+  networking.interfaces = {
+    "${lan_port}" = {
+      ipv4.address = lib.mkOverride 0 [ ];
+    };
+    "${wan_port}" = {
+      ipv4.address = lib.mkOverride 0 [ ];
+    };
+    "${iot_port}" = {
+      ipv4.address = lib.mkOverride 0 [ ];
+    };
+  };
+  services.dnsmasq = lib.optionalAttrs config.services.hostapd.enable {
     enable = true;
     extraConfig = ''
       interface=${lan_port}
