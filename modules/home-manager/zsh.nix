a97c3e1e293bfbf35daa720b9fdaf5b3bd4c8aea
Edward Hesketh <headblockhead@gmail.com>
2024-02-14 16:33:13 +0000

    chore: remove unnecessary plugin from home-manager zsh config

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ ... }: {
   programs.zsh = {
     enable = true;
     initExtra = ''
@@ -6,17 +6,5 @@
       export PATH="$GOBIN:$PATH"
       export ZSH_HIGHLIGHT_STYLES[comment]=fg=245,bold
     '';
-    plugins = [
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.5.0";
-          sha256 = "IT3wpfw8zhiNQsrw59lbSWYh0NQ1CUdUtFzRzHlURH0=";
-        };
-      }
-    ];
   };
 }
