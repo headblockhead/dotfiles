b5cc25c3c7c7d4cb925e62a1b15842412eb24b16
Edward Hesketh <headblockhead@gmail.com>
2024-02-15 00:39:08 +0000

    feat: fix local cache

--- modules/nixos/cachesLocal.nix
+++ modules/nixos/cachesLocal.nix
@@ -1,11 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   environment.systemPackages = with pkgs; [
     attic
   ];
 
   nix.settings = {
-    substituters = [ "http://192.168.1.1:8080/global" ];
+    substituters = lib.mkBefore [ "http://192.168.1.1:8080/global" ]; # This applies only if the user had no nix.conf in their home directory
+    trusted-substituters = [ "http://192.168.1.1:8080/global" ];
     trusted-public-keys = [ "global:qjMzgEexv/Bq5JGqNIyndj4OPEBrXG9zqPCVZHV30zk=" ];
   };
 }
