8baf3dcf0417584f150e0adf9789d8c2507ef02b
Edward Hesketh <headblockhead@gmail.com>
2024-03-10 21:12:03 +0000

    feat: update cacheslocal to match new cache

--- modules/nixos/cachesLocal.nix
+++ modules/nixos/cachesLocal.nix
@@ -5,8 +5,8 @@
   ];
 
   nix.settings = {
-    substituters = [ "http://192.168.1.1/global?priority=10" ];
-    trusted-substituters = [ "http://192.168.1.1/global?priority=10" ];
+    substituters = [ "http://192.168.1.1?priority=10" ];
+    trusted-substituters = [ "http://192.168.1.1?priority=10" ];
     trusted-public-keys = [ "h6cache:N9Ghc9O46jxUCwsN7JDFomuobHwUOls7tiSFKjHo2Gs=" ];
   };
 }
