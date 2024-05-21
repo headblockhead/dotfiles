b24f23b25d5c511bb338d595f22fd8df06dc41f8
Edward Hesketh <headblockhead@gmail.com>
2024-05-22 00:08:53 +0100

    fix: miner uses p2pool

--- modules/nixos/xmrig.nix
+++ modules/nixos/xmrig.nix
@@ -18,16 +18,16 @@
       cuda = false;
       pools = [
         # MoneroOcean
-        {
-          url = "gulf.moneroocean.stream:20128";
-          user = "48pEZBjqjNRCbVptEMGRdeYeUgmXaHbz7gLErTneJnN8Uc5r2qHyEPoGmS1NSmQqaK5hUjZUvRG24jBNRKjA51qbDkWM1oX";
-          keepalive = true;
-          tls = true;
-        }
+        #{
+        #  url = "gulf.moneroocean.stream:20128";
+        #  user = "48pEZBjqjNRCbVptEMGRdeYeUgmXaHbz7gLErTneJnN8Uc5r2qHyEPoGmS1NSmQqaK5hUjZUvRG24jBNRKjA51qbDkWM1oX";
+        #  keepalive = true;
+        #  tls = true;
+        #}
         # P2Pool
-        #        {
-        #          url = "192.168.1.6:3333";
-        #        }
+        {
+          url = "192.168.1.6:3333";
+        }
       ];
     };
   };
