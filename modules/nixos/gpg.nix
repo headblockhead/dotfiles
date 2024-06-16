b3385b9d2d60e8a790b0631f109232a476c82d5a
Edward Hesketh <headblockhead@gmail.com>
2024-06-16 01:29:03 +0100

    feat: update to 24.05

--- modules/nixos/gpg.nix
+++ modules/nixos/gpg.nix
@@ -10,9 +10,7 @@
 
   programs.gnupg.agent = {
     enable = true;
-    pinentryFlavor = "curses"; # works in console interactive only, does not work with GUI apps.
-    # pinentryFlavor = "gtk2"; # creates a gtk popup
-    # pinentryFlavor = "gnome3"; # creates a gnome popup.
+    pinentryPackage = pkgs.pinentry-tty;
     enableSSHSupport = true;
   };
 }
--- systems/edward-desktop-01/hardware.nix
+++ systems/edward-desktop-01/hardware.nix
@@ -21,10 +21,11 @@
   hardware.nvidia = {
 
     # Modesetting is required.
-    modesetting.enable = false;
+    modesetting.enable = true;
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    #powerManagement.enable = true;
+    powerManagement.enable = true;
+
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
