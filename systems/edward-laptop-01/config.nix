b6071bc2ba343dcd2da39d381697576d3069256b
Edward Hesketh <headblockhead@gmail.com>
2024-06-28 23:53:49 +0100

    feat: update apps fav.

--- systems/edward-laptop-01/config.nix
+++ systems/edward-laptop-01/config.nix
@@ -87,7 +87,7 @@
   # find / -name '*.desktop' 2> /dev/null
   services.xserver.desktopManager.gnome.favoriteAppsOverride = ''
     [org.gnome.shell]
-    favorite-apps=[ 'firefox.desktop', 'torbrowser.desktop', 'org.gnome.Terminal.desktop' 'org.gnome.Nautilus.desktop', 'org.gnome.Settings.desktop','org.gnome.Calculator.desktop', 'org.rncbc.qjackctl.desktop', 'ardour8.desktop', 'vlc.desktop', 'audacity.desktop' ]
+    favorite-apps=[ 'firefox.desktop', 'torbrowser.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Settings.desktop','org.gnome.Calculator.desktop', 'org.rncbc.qjackctl.desktop', 'ardour8.desktop', 'vlc.desktop', 'audacity.desktop' ]
   '';
 
   # This value determines the NixOS release from which the default
