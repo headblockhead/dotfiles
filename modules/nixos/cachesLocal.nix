54e33b811ca73557dabded35198c541fdfec38c0
Edward Hesketh <headblockhead@gmail.com>
2024-08-05 19:55:15 +0100

    feat: update public cache key

--- modules/nixos/cachesLocal.nix
+++ modules/nixos/cachesLocal.nix
@@ -3,6 +3,6 @@
   nix.settings = {
     extra-substituters = [ "http://192.168.1.1?priority=10" ]; # extra-substituters is for non-reliable substituters to prevent build failures.
     trusted-substituters = [ "http://192.168.1.1?priority=10" ];
-    trusted-public-keys = [ "h6cache:N9Ghc9O46jxUCwsN7JDFomuobHwUOls7tiSFKjHo2Gs=" ];
+    trusted-public-keys = [ "h6cache:03zxq07gkKt7szy/RFQHfnylEgS620rT/csKSjCX6/Q=" ];
   };
 }
