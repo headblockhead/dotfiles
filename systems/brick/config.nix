b68f1f7fe4336ab620d1a863bab69a212ef903cc
Edward Hesketh <headblockhead@gmail.com>
2024-10-03 14:44:50 +0100

    Create basic brick config

---
+++ systems/brick/config.nix
@@ -0,0 +1,73 @@
+{ inputs, outputs, lib, config, pkgs, agenix, ... }:
+
+{
+  networking.hostName = "brick";
+
+  imports = with outputs.nixosModules; [
+    basicConfig
+    bluetooth
+    bootloaderGraphical
+    cachesGlobal
+    desktop
+    development
+    docker
+    fileSystems
+    fonts
+    fzf
+    git
+    gpg
+    homeManager
+    network
+    sound
+    ssh
+    users
+    zsh
+  ];
+
+  networking.firewall.enable = lib.mkForce false;
+
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+    config = {
+      allowUnfree = true;
+      cudaSupport = true;
+    };
+  };
+
+  # This will add each flake input as a registry
+  # To make nix3 commands consistent with your flake
+  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+  # This will additionally add your inputs to the system's legacy channels
+  # Making legacy nix commands consistent as well, awesome!
+  nix.nixPath = [ "/etc/nix/path" ];
+  environment.etc =
+    lib.mapAttrs'
+      (name: value: {
+        name = "nix/path/${name}";
+        value.source = value.flake;
+      })
+      config.nix.registry;
+
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    auto-optimise-store = true;
+  };
+
+  # Extra packages to install
+  environment.systemPackages = [
+    pkgs.xc
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+}
