5ec5fe8ac91951433c77837ddbb44862ac2af91c
Edward Hesketh <headblockhead@gmail.com>
2024-10-08 14:57:02 +0100

    Update hardware.nix

--- systems/brick/hardware.nix
+++ systems/brick/hardware.nix
@@ -5,11 +5,5 @@
     [
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
-    virtualisation.virtualbox.guest = {
-enable = true;
-seamless = true;
-draganddrop = true;
-clipboard = true;
-};
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
