e4601e4eac4a1d68e75c6dbcceca12c0d7abecc2
Edward Hesketh <inbox@edwardh.dev>
2024-11-18 00:37:37 +0000

    feat: add sddm

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -1,8 +1,16 @@
 { pkgs, ... }: {
   programs.hyprland.enable = true;
 
+  services.displayManager.sddm = {
+    package = pkgs.kdePackages.sddm;
+    enable = true;
+    enableHidpi = true;
+    theme = "where_is_my_sddm_theme";
+  };
+
   environment.systemPackages = [
     pkgs.kitty
+    pkgs.where-is-my-sddm-theme.override
   ];
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
