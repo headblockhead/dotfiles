201d8e1f9d3faa09f6c7f24f3808e26b860645fc
Edward Hesketh <inbox@edwardh.dev>
2024-11-22 15:55:50 +0000

    chore: delete unused nvidia config

--- systems/edward-desktop-01/hardware.nix
+++ systems/edward-desktop-01/hardware.nix
@@ -6,35 +6,22 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ]; # Various modules for booting and reading drives.
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
 
+  # AMD GPU
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
   };
-
-  # Load nvidia driver for Xorg and Wayland
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
-
   environment.systemPackages = with pkgs; [
     lact
-    ddcutil
-    gnomeExtensions.brightness-control-using-ddcutil
   ];
-
-  # NVIDIA
-  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
-  hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-    open = false;
-    powerManagement.enable = true;
-    modesetting.enable = true;
-  };
-
-  # AMD
   systemd.packages = with pkgs; [ lact ];
   systemd.services.lactd.wantedBy = [ "multi-user.target" ];
+
+  # ROCm
   systemd.tmpfiles.rules = [
     "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
   ];
