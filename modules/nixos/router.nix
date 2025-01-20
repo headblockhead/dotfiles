d2ee0a8e8f731325c5a8bfa39a4c87ce4eb32e76
Edward Hesketh <inbox@edwardh.dev>
2025-01-20 01:45:51 +0000

    feat: remove unused timeserver

--- modules/nixos/router.nix
+++ modules/nixos/router.nix
@@ -73,13 +73,6 @@ in
       '';
     };
   };
-  services.chrony = {
-    enable = true;
-    initstepslew.enabled = true;
-    enableRTCTrimming = true;
-    enableNTS = true;
-    servers = [ "0.pool.ntp.org" "1.pool.ntp.org" "2.pool.ntp.org" "3.pool.ntp.org" ];
-  };
   services.avahi = {
     enable = true;
     domainName = "local";
