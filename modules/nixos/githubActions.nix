ffc52933c8c83405f34a2cee26ba3375b522368c
Edward Hesketh <inbox@edwardh.dev>
2025-02-09 16:37:17 +0000

    chore: remove squirrel from gh actions

--- modules/nixos/githubActions.nix
+++ modules/nixos/githubActions.nix
@@ -2,12 +2,6 @@
 {
   age.secrets.github-actions-token.file = ../../secrets/github-actions-token.age;
   services.github-runners = {
-    squirrel = {
-      enable = true;
-      ephemeral = true;
-      url = "https://github.com/headblockhead/squirrel";
-      tokenFile = config.age.secrets.github-actions-token.path;
-    };
     slab-firmware = {
       enable = true;
       ephemeral = true;
