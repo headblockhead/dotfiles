28479b7cf6324580b7e75a4305b9b318be096ad2
Edward Hesketh <inbox@edwardh.dev>
2025-04-09 16:11:38 +0100

    fix: use correct ethernet drivers

--- systems/gateway/hardware.nix
+++ systems/gateway/hardware.nix
@@ -7,7 +7,12 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.extraModulePackages = [ ];
+
+  boot.blacklistedKernelModules = [ "r8169" ];
+  boot.kernelModules = [ "r8125" ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    r8125
+  ];
 
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
