4781cd033b2c0fd5cdeaafab23a7bf3232e85962
Edward Hesketh <inbox@edwardh.dev>
2025-04-19 15:43:01 +0100

    feat: add bluetooth to rpi4-01

--- systems/rpi4-01/config.nix
+++ systems/rpi4-01/config.nix
@@ -15,10 +15,14 @@
 
   networking.firewall.allowedTCPPorts = [ 8123 ];
 
+  hardware.bluetooth.enable = true;
   virtualisation.oci-containers = {
     backend = "podman";
     containers.homeassistant = {
-      volumes = [ "home-assistant:/config" ];
+      volumes = [
+        "home-assistant:/config"
+        "/run/dbus:/run/dbus:ro"
+      ];
       environment.TZ = "Europe/London";
       image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
       extraOptions = [
